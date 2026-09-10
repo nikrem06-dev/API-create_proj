774a3ae6393a5c4ca14e7341fc1fb005de393ae5
nikrem06-dev <nikrem06@gmail.com>
2026-09-10 23:29:28 +0800

    Added logging

--- pipeline.py
+++ pipeline.py
@@ -1,32 +1,40 @@
 import requests
 import json
 import csv
+import logging
+logging.basicConfig(filename='logs/pipeline.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def fetch_data(path):
     try:
+        logging.info(f"Fetching data from {path}")
         response = requests.get(path, timeout=10)
         data = response.json()
+        logging.info(f"Data fetched successfully from {path}")
         return data
     except requests.RequestException as e:
-        print(f"Error fetching data from {path}: {e}")
+        logging.error(f"Error fetching data from {path}: {e}")
         return None
 
 def validate_data(data):
+    logging.info("Validating data")
     if not isinstance(data[:1], list):
-        print("Data is not a list.")
+        logging.error("Data is not a list.")
         return False
     for row in data:
         if row[0] is None:
-            print(f"Invalid data found in row: {row}")
+            logging.error(f"Invalid data found in row: {row}")
             return False
         if row[1] is None:
-            print(f"Invalid data found in row: {row}")
+            logging.error(f"Invalid data found in row: {row}")
             return False
         if row[2] is None or row[2].get('key') is None:
-            print(f"Invalid data found in row: {row}")
+            logging.error(f"Invalid data found in row: {row}")
             return False
+    logging.info("Data validation completed successfully.")
     return True
 
 def transform_data(data):
+    logging.info("Transforming data")
     transformed_data = []
     for row in data:
         if 'UserId' in row and 'Id' in row and 'Title' in row:
@@ -35,17 +43,22 @@ def transform_data(data):
                 'Id': row['Id'],
                 'Title': row['Title'],
             })
+    logging.info("Data transformation completed successfully.")
     return transformed_data
 
 def save_json(data, filename):
+    logging.info(f"Saving data to {filename} as JSON")
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f)
+    logging.info(f"Data saved to {filename} successfully.")
 
 def save_csv(data, filename):
+    logging.info(f"Saving data to {filename} as CSV")
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.DictWriter(f, fieldnames=data[0].keys())
         writer.writeheader()
         writer.writerows(data)
+    logging.info(f"Data saved to {filename} successfully.")
 
 path = "https://dummyjson.com/"
 
